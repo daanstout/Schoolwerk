ecea9a62c4d9fbe5b222536e590b3a75f5400c3a
maestroke <formaestroke@gmail.com>
2017-09-08 14:26:34 +0200

    begin singly linked list

---
+++ AD/SInglyLinkedList/SInglyLinkedList/Node.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SInglyLinkedList {
+    public class Node<T> {
+        public T data;
+        public Node<T> next;
+
+        public Node() { }
+
+        public Node(T data) {
+            this.data = data;
+        }
+
+        public Node(T data, Node<T> next) {
+            this.data = data;
+            this.next = next;
+        }
+    }
+}
