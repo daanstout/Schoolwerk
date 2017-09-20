eea8673dbb60d3278cf8743c8f99146487ea6f06
maestroke <formaestroke@gmail.com>
2017-09-20 16:28:33 +0200

    gewerkt aan AD

---
+++ AD/AD_Week2_HW/AD_Week2_HW/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AD_Week2_HW {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine(faculteit(4));
+            Console.WriteLine();
+            Console.WriteLine(omenom(5));
+            Console.WriteLine(omenom(6));
+            Console.WriteLine(omenom(7));
+            Console.WriteLine();
+            Console.WriteLine(aantalEnen(7));
+            Console.WriteLine(aantalEnen(9));
+            Console.WriteLine();
+            Console.WriteLine(binair(7));
+            Console.WriteLine(binair(9));
+            Console.WriteLine(binair(100));
+            Console.WriteLine();
+            Console.WriteLine(Decimal("1001"));
+            Console.WriteLine(Decimal("111"));
+            Console.WriteLine(Decimal("101100101010101001010101001"));
+
+            Console.ReadKey();
+        }
+
+        static int faculteit(int i) {
+            if (i <= 1) {
+                return 1;
+            } else {
+                return i * faculteit(i - 1);
+            }
+        }
+
+        static int omenom(int i) {
+            if (i < 0) {
+                return 0;
+            } else if (i <= 2) {
+                return i;
+            } else {
+                return i + omenom(i - 2);
+            }
+        }
+
+        static int aantalEnen(int i) {
+            if (i < 0) {
+                return 0;
+            } else if (i == 1) {
+                return 1;
+            } else if (i % 2 == 0) {
+                return aantalEnen(i / 2);
+            } else if (i % 2 == 1) {
+                return aantalEnen(i / 2) + 1;
+            }
+            return 0;
+        }
+
+        static string binair(int i) {
+            if (i < 0) {
+                return "";
+            } else if (i == 0) {
+                return "0";
+            } else if (i == 1) {
+                return "1";
+            } else if (i % 2 == 0) {
+                return binair(i / 2) + "0";
+            } else if (i % 2 == 1) {
+                return binair(i / 2) + "1";
+            }
+            return "";
+        }
+
+        static int Decimal(string s) {
+            if (s == "")
+                return 0;
+            if (s[0] == '1') {
+                return (1 << s.Length - 1) + Decimal(s.Substring(1));
+            } else if (s[0] == '0') {
+                return Decimal(s.Substring(1));
+            }
+            return 0;
+        }
+    }
+}
---
+++ AD/AD_Week2_HW/AD_Week2_HW_Form/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AD_Week2_HW_Form {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+            pictureBox1.Invalidate();
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e) {
+            base.OnPaint(e);
+            {
+                HTree(e.Graphics, new Point(350, 350), new Size(200, 200), 7);
+                SierpinskiTree(e.Graphics, new Point(350, 350), new Size(400, 400), 7);
+                //BoomVanPythagoras(e.Graphics);
+            }
+        }
+
+        private void HTree(Graphics g, Point location, Size size, int amount) {
+            if (amount > 0) {
+                g.DrawLine(new Pen(Color.Black), new Point(location.X - (size.Width / 2), location.Y), new Point(location.X + (size.Width / 2), location.Y));
+                g.DrawLine(new Pen(Color.Black), new Point(location.X - (size.Width / 2), location.Y - (size.Height / 2)), new Point(location.X - (size.Width / 2), location.Y + (size.Height / 2)));
+                g.DrawLine(new Pen(Color.Black), new Point(location.X + (size.Width / 2), location.Y - (size.Height / 2)), new Point(location.X + (size.Width / 2), location.Y + (size.Height / 2)));
+                HTree(g, new Point(location.X - size.Width / 2, location.Y - size.Height / 2), new Size(size.Width / 2, size.Height / 2), amount - 1);
+                HTree(g, new Point(location.X + size.Width / 2, location.Y - size.Height / 2), new Size(size.Width / 2, size.Height / 2), amount - 1);
+                HTree(g, new Point(location.X - size.Width / 2, location.Y + size.Height / 2), new Size(size.Width / 2, size.Height / 2), amount - 1);
+                HTree(g, new Point(location.X + size.Width / 2, location.Y + size.Height / 2), new Size(size.Width / 2, size.Height / 2), amount - 1);
+            }
+        }
+
+        private void SierpinskiTree(Graphics g, Point location, Size size, int amount) {
+            if(amount > 0) {
+                PointF[] points = new PointF[3];
+                points[0] = new PointF(location.X - size.Width / 2, location.Y + size.Height / 2);
+                points[1] = new PointF(location.X + size.Width / 2, location.Y + size.Height / 2);
+                points[2] = new PointF(location.X, location.Y - size.Height / 2);
+                g.DrawPolygon(new Pen(Color.Black), points);
+
+                SierpinskiTree(g, new Point(location.X, location.Y - size.Height / 4), new Size(size.Width / 2, size.Height / 2), amount - 1);
+                SierpinskiTree(g, new Point(location.X - size.Width / 4, location.Y + size.Height / 4), new Size(size.Width / 2, size.Height / 2), amount - 1);
+                SierpinskiTree(g, new Point(location.X + size.Width / 4, location.Y + size.Height / 4), new Size(size.Width / 2, size.Height / 2), amount - 1);
+            }
+        }
+
+        private void BoomVanPythagoras(Graphics g, Point location, Size size, int amount) {
+
+        }
+    }
+}
