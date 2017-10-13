536ffc387459d1c52ffb2417f985dcdf9d292cfb
maestroke <formaestroke@gmail.com>
2017-10-13 13:56:16 +0200

    hashtables

---
+++ AD/HashTable/HashTable/HashTable.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTable {
+    public class HashTable {
+        int[] table;
+
+        public HashTable() {
+            table = new int[11];
+        }
+
+        public void insert(int x) {
+            table[Hash(x)] = x;
+        }
+
+        private int Hash(int x) {
+            return (x * 7) % 11;
+        }
+
+        public void PrintTable() {
+            for(int i = 0; i < table.Length; i++) {
+                Console.WriteLine("index " + i + " contains: " + table[i]);
+            }
+        }
+    }
+}
---
+++ AD/HashTable/HashTable/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTable {
+    class Program {
+        static void Main(string[] args) {
+            HashTable table = new HashTable();
+
+            table.insert(2);
+            table.insert(4);
+            table.insert(5);
+            table.insert(1);
+            table.insert(4);
+            table.insert(14);
+            table.insert(12);
+
+            table.PrintTable();
+
+
+            Console.ReadKey();
+        }
+    }
+}
