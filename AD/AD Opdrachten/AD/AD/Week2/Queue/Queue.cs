75dc484c56c9b872baa632bf78e3f8a53c6cf065
Stonebro <anne_zweers@live.nl>
2017-10-31 21:31:37 +0100

    Comments I think?

--- AD/AD Opdrachten/AD/AD/Week2/Queue/Queue.cs
+++ AD/AD Opdrachten/AD/AD/Week2/Queue/Queue.cs
@@ -12,39 +12,38 @@ namespace AD.Week2.Queue {
         public Queue() {
             queueMaxSize = 5;
             queue = new T[queueMaxSize];
-            front = back = queueSize = 0;
+            front = back = queueSize = 0; // Optional
         }
 
         public void Enqueue(T item) {
-            if (queueSize == queueMaxSize)
+            if (queueSize == queueMaxSize) // Als de array vol zit, verdubbel dan de grootte
                 DoubleQueue();
-            queue[back] = item;
+            queue[back] = item; // Zet het item achteraan de queue
             back++;
             queueSize++;
-            back %= queueMaxSize;
+            back %= queueMaxSize; // Back wordt weer 0 als hij het einde van de array bereikt.
         }
 
         public T Dequeue() {
             T item = queue[front];
-            queue[front] = default(T);
-            queueSize--;
-            front++;
-            front %= queueMaxSize;
+            queue[front] = default(T); // Zet de front op de default waarde (0, null, w/e)
+            queueSize--; // Queue wordt kleiner
+            front++; // De front gaat naar voren
+            front %= queueMaxSize; // Front wordt weer 0 als hij het einde van de array bereikt.
             return item;
         }
 
         public void makeEmpty() {
             for(int i = 0; i < queueMaxSize; i++) {
-                queue[i] = default(T);
+                queue[i] = default(T); // Zet elk element van de array op de default waarde (0, null, w/e)
             }
-
-            front = back = queueSize = 0;
+            front = back = queueSize = 0; // Reset alle posities
         }
 
         private void DoubleQueue() {
-            T[] temp = new T[queueMaxSize * 2];
+            T[] temp = new T[queueMaxSize * 2]; // Verdubbel de grootte
             for(int i = 0; i < queueMaxSize; i++) {
-                temp[i] = queue[(i+back)%queueMaxSize];
+                temp[i] = queue[(i+back)%queueMaxSize]; // Vul de nieuwe array zo, dat de front vooraan staat en de back achteraan.
             }
             back = queueMaxSize;
             front = 0;
--- AD/AD Opdrachten/AD/AD/Week2/Stack/Stack.cs
+++ AD/AD Opdrachten/AD/AD/Week2/Stack/Stack.cs
@@ -15,16 +15,16 @@ namespace AD.Week2.Stack {
             top = stackSize = 0;
         }
 
-        public T Pop() {
-            top = stackSize -= 1;
-            return stack[top + 1];
+        public T Pop() { // Pop: Verwijder het bovenste element van de stack
+            top = stackSize -= 1; // Set de top op het element ónder de top. De oude top wordt vanzelf verwijderd door de Garbage Collector
+            return stack[top + 1]; // Return het element boven de nieuwe top.
         }
 
         public void Push(T data) {
-            if (stackSize == maxStackSize)
+            if (stackSize == maxStackSize) // Als de array vol is, verdubbel dan de grootte
                 DoubleStack();
-            top = stackSize += 1;
-            stack[top] = data;
+            top = stackSize += 1; // Vergroot de index met 1
+            stack[top] = data; // Zet de data op deze index
         }
 
         public T Top() {
@@ -32,11 +32,11 @@ namespace AD.Week2.Stack {
         }
 
         private void DoubleStack() {
-            T[] temp = new T[maxStackSize * 2];
+            T[] temp = new T[maxStackSize * 2]; // Nieuwe lege array, 2x zo groot
             for (int i = 0; i < maxStackSize; i++)
-                temp[i] = stack[i];
+                temp[i] = stack[i]; // Vul de array
             maxStackSize *= 2;
-            stack = temp;
+            stack = temp; // Vervang de oude array
         }
     }
 }
