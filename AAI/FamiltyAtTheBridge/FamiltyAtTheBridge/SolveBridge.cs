653631d87b007e62986c711903bebdd8f5a5054c
maestroke <formaestroke@gmail.com>
2018-02-05 20:21:39 +0100

    finished SolveBridge

--- AAI/FamiltyAtTheBridge/FamiltyAtTheBridge/SolveBridge.cs
+++ AAI/FamiltyAtTheBridge/FamiltyAtTheBridge/SolveBridge.cs
@@ -7,50 +7,102 @@ using System.Threading.Tasks;
 namespace FamiltyAtTheBridge {
     public class SolveBridge {
         readonly int lampTime = 30;
-        //bool lampCrossed;
-        //int timeSpent = 0;
-        //Person[] family;
 
-        public SolveBridge() {
-            //family = new Person[5] { new Person(1), new Person(3), new Person(6), new Person(8), new Person(12) };
-        }
+        public SolveBridge() { }
 
         public void Solve() {
-            //Solve("", new Person[5] { new Person(1), new Person(3), new Person(6), new Person(8), new Person(12) }, 0, false);
-            List<int> ints = GetAllMoves(new Person[5] { new Person(1), new Person(3), new Person(6), new Person(8), new Person(12) }, 0, false);
-            foreach (int i in ints)
-                Console.WriteLine(i);
+            Solve("", new Person[5] { new Person(1), new Person(3), new Person(6), new Person(8), new Person(12) }, 0, false, 0);
         }
 
-        private void Solve(string solution, Person[] family, int timeSpent, bool lampCrossed) {
-            if (IsSolved(family))
-                PrintSolution(solution);
+        private void Solve(string solution, Person[] family, int timeSpent, bool lampCrossed, int steps) {
+            if (IsSolved(family)) {
+                PrintSolution(solution, timeSpent, steps);
+                return;
+            }
 
             List<int> moves = GetAllMoves(family, timeSpent, lampCrossed);
 
-            foreach(int move in moves) {
+            foreach (int move in moves) {
                 if (lampCrossed) {
+                    int p = 0;
                     switch (move) {
                         case 1:
-                            family[0].crossed = true;
-                            solution += "--> person@speed" + family[0].speed + "\n";
-                            Solve(solution, family, timeSpent + family[0].speed, !lampCrossed);
+                            p = 0;
                             break;
                         case 2:
+                            p = 1;
                             break;
                         case 4:
+                            p = 2;
                             break;
                         case 8:
+                            p = 3;
                             break;
                         case 16:
+                            p = 4;
                             break;
+                        default:
+                            continue;
                     }
+                    family[p].crossed = false;
+                    Solve(solution + "--> person@speed" + family[p].speed + "\n", family, timeSpent + family[p].speed, !lampCrossed, steps + 1);
+                    family[p].crossed = true;
+                } else {
+                    int p1 = 0;
+                    int p2 = 0;
+                    switch (move) {
+                        case 3:
+                            p1 = 0;
+                            p2 = 1;
+                            break;
+                        case 5:
+                            p1 = 0;
+                            p2 = 2;
+                            break;
+                        case 6:
+                            p1 = 1;
+                            p2 = 2;
+                            break;
+                        case 9:
+                            p1 = 0;
+                            p2 = 3;
+                            break;
+                        case 10:
+                            p1 = 1;
+                            p2 = 3;
+                            break;
+                        case 12:
+                            p1 = 2;
+                            p2 = 3;
+                            break;
+                        case 17:
+                            p1 = 0;
+                            p2 = 4;
+                            break;
+                        case 18:
+                            p1 = 1;
+                            p2 = 4;
+                            break;
+                        case 20:
+                            p1 = 2;
+                            p2 = 4;
+                            break;
+                        case 24:
+                            p1 = 3;
+                            p2 = 4;
+                            break;
+                        default:
+                            continue;
+                    }
+                    family[p1].crossed = family[p2].crossed = true;
+                    Solve(solution + "<-- person@speed" + family[p1].speed + " + person@speed" + family[p2].speed + "\n", family, timeSpent + family[p2].speed, !lampCrossed, steps + 1);
+                    family[p1].crossed = family[p2].crossed = false;
                 }
             }
         }
 
-        private void PrintSolution(string solution) {
-            Console.WriteLine(solution);
+        private void PrintSolution(string solution, int timeSpent, int steps) {
+            Console.WriteLine(solution + "Solution in " + steps + " steps using " + timeSpent + " lamp energy.\n\n");
         }
 
         private bool IsSolved(Person[] family) {
@@ -75,8 +127,7 @@ namespace FamiltyAtTheBridge {
                 for (int j = i + 1; j < 5; j++) {
                     if (family[j].crossed)
                         continue;
-                    Console.WriteLine(i + " - " + j);
-                    if (timeSpent + family[j].speed < lampTime) {
+                    if (timeSpent + family[j].speed <= lampTime) {
                         int toAdd = (1 << i) + (1 << j);
                         crossings.Add(toAdd);
                     }
@@ -90,7 +141,7 @@ namespace FamiltyAtTheBridge {
             for (int i = 0; i < 5; i++) {
                 if (!family[i].crossed)
                     continue;
-                if (timeSpent + family[i].speed < lampTime)
+                if (timeSpent + family[i].speed <= lampTime)
                     crossings.Add(1 << i);
             }
             return crossings;
---
+++ AAI/Snake/Snake/Snake.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace Snake {
+    partial class Snake {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.snakePictureBox = new System.Windows.Forms.PictureBox();
+            this.longestPathButton = new System.Windows.Forms.Button();
+            this.longestPathLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.snakePictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // snakePictureBox
+            // 
+            this.snakePictureBox.Location = new System.Drawing.Point(12, 12);
+            this.snakePictureBox.Name = "snakePictureBox";
+            this.snakePictureBox.Size = new System.Drawing.Size(260, 205);
+            this.snakePictureBox.TabIndex = 0;
+            this.snakePictureBox.TabStop = false;
+            // 
+            // longestPathButton
+            // 
+            this.longestPathButton.Location = new System.Drawing.Point(12, 226);
+            this.longestPathButton.Name = "longestPathButton";
+            this.longestPathButton.Size = new System.Drawing.Size(102, 23);
+            this.longestPathButton.TabIndex = 1;
+            this.longestPathButton.Text = "Longest Path";
+            this.longestPathButton.UseVisualStyleBackColor = true;
+            // 
+            // longestPathLabel
+            // 
+            this.longestPathLabel.AutoSize = true;
+            this.longestPathLabel.Location = new System.Drawing.Point(120, 231);
+            this.longestPathLabel.Name = "longestPathLabel";
+            this.longestPathLabel.Size = new System.Drawing.Size(69, 13);
+            this.longestPathLabel.TabIndex = 2;
+            this.longestPathLabel.Text = "Longest path";
+            // 
+            // Snake
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.longestPathLabel);
+            this.Controls.Add(this.longestPathButton);
+            this.Controls.Add(this.snakePictureBox);
+            this.Name = "Snake";
+            this.Text = "Snake";
+            ((System.ComponentModel.ISupportInitialize)(this.snakePictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox snakePictureBox;
+        private System.Windows.Forms.Button longestPathButton;
+        private System.Windows.Forms.Label longestPathLabel;
+    }
+}
+
